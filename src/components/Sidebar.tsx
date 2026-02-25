740733788f026524c488da4ca1de1d44ed625c4c
PhuongDev <trminhphuong28@gmail.com>
2026-02-25 13:51:09 +0700

    test

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { UserProfile } from '../types';
 import { generateWeaknessAdvice, isApiKeyConfigured } from '../services/geminiApi';
 
